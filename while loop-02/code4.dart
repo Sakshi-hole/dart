c3bbbdc982163d3d6cc30ba5e064b276c16f109e
Sakshi-hole <sakshihole2105@gmail.com>
2024-08-22 01:28:56 +0530

    while loop

---
+++ while loop-02/code4.dart
@@ -0,0 +1,13 @@
+/*Write a program to print a the number divisible by 7 in
+range 20 to 30. */
+
+void main() {
+  int start = 20;
+  int end = 30;
+  while (start <= end) {
+    if (start % 7 == 0) {
+      print(start);
+      }
+      start++;
+      }
+      }
