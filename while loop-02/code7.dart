25bd9dc1d23fe4608fceb7c1d65f2172b2964943
Sakshi-hole <sakshihole2105@gmail.com>
2024-08-22 01:29:38 +0530

    while loop

---
+++ while loop-02/code7.dart
@@ -0,0 +1,14 @@
+//7: Write a program to count the Odd digits of the given number. 942111423
+
+void main(){
+  int number= 942111423;
+  int count=0;
+  while(number>0){
+    int digit = number%10;
+    if(digit %2!=0){
+      count++;
+      }
+      number=number~/10;
+}
+    print(count);
+}
