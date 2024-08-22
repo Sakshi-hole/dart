15e46bc0da4ccdb2cfd5333fb76ee05f80471573
Sakshi-hole <sakshihole2105@gmail.com>
2024-08-22 15:40:50 +0530

    if else

---
+++ if-else statement-2/code10.dart
@@ -0,0 +1,11 @@
+// Write a dart program to check whether you are eligible for campus placements:
+
+void main(){
+  double percentage=70;
+  double cgpa=7;
+  if(percentage>=70 && cgpa >=7.0){
+    print("You are eligible");
+    }else{
+      print("You are not eligible");
+      }
+}
