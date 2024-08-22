74ab4b29e4194d1d81b17b97391e4736dcc6110f
Sakshi-hole <sakshihole2105@gmail.com>
2024-08-22 15:40:03 +0530

    if else

---
+++ if-else statement-2/code8.dart
@@ -0,0 +1,10 @@
+// Create a dart program to find the correct place for parking in core2web parking.
+
+void main(){
+  String vehicle="Bike";
+  if(vehicle=="Bike"){
+    print("Go to Parking 2");
+    }else{
+      print("Go to Parking 1");
+      }
+    }
