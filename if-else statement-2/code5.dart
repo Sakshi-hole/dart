58f63cfea7e78bab01c10a13cbe651d96890aaeb
Sakshi-hole <sakshihole2105@gmail.com>
2024-08-22 15:38:59 +0530

    if else

---
+++ if-else statement-2/code5.dart
@@ -0,0 +1,10 @@
+// Write a dart program to check if a number when divided by 3 the remainder is 2 or less than 2.
+
+void main(){
+  int x=15;
+  if(x%3==2){
+    print("Remainder is equal to 2");
+    }else{
+      print("remainder is less than 2");
+      }
+    }
