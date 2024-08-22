2d96108f7861db21532e0fef9c74812a074dd776
Sakshi-hole <sakshihole2105@gmail.com>
2024-08-22 15:30:55 +0530

    if else

--- if-else statement-1/code2.dart
+++ if-else statement-1/code2.dart
@@ -1,17 +1,14 @@
-///Program 1: Write a dart program to check if a number is even or odd.
+// Write a program , take a number and print whether it is less than 10 or greater than 10.
 
 void main() {
-  var x = 10;
-  if (x % 2 == 0) {
-    print("10 is an even number");
+  int x = 1;
+  if (x > 10) {
+    print("x is greater than 10");
   } else {
-    print("10 is an odd number");
-  }
-
-  var y = 37;
-  if (y % 2 == 0) {
-    print("37 is an even number");
-  } else {
-    print("37 is an odd number");
+    if (x == 10) {
+      print("x is equal to 10");
+    } else {
+      print("x is less than 10");
+    }
   }
 }
