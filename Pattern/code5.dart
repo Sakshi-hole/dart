d90f79a0d4d24b1dc51256aebcf2d16ec101a98d
Sakshi-hole <sakshihole2105@gmail.com>
2024-08-22 15:43:37 +0530

    pattern

---
+++ Pattern/code5.dart
@@ -0,0 +1,26 @@
+/* Write a program to print the following pattern
+Number of rows = 3
+3 3 3
+2 2 2
+1 1 1
+
+Number of rows = 4
+4 4 4 4
+3 3 3 3
+2 2 2 2
+1 1 1 1 */
+
+import "dart:io";
+
+void main() {
+  print("Enter Number of rows:");
+  int rows = int.parse(stdin.readLineSync()!);
+  int number = rows;
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < rows; j++) {
+      stdout.write("$number ");
+    }
+    print("");
+    number--;
+  }
+}
