83bcdb234006485849f782164dc8c01f8f47bcd5
Sakshi-hole <sakshihole2105@gmail.com>
2024-08-22 15:32:07 +0530

    if else

---
+++ if-else statement-1/code4.dart
@@ -0,0 +1,12 @@
+// Write a program take a number and print whether it is positive or negative.
+
+void main() {
+  int num = 10;
+  if (num > 0) {
+    print("number is positive");
+  } else if (num == 0) {
+    print("number is zero");
+  } else {
+    print("number is negative");
+  }
+}
