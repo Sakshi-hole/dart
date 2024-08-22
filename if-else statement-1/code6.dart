105bc7e7900d071f339f5f23b9d064db3f8ac9c0
Sakshi-hole <sakshihole2105@gmail.com>
2024-08-22 15:33:40 +0530

    if else

---
+++ if-else statement-1/code6.dart
@@ -0,0 +1,26 @@
+//Write a dart program that takes a number from 0 to 5 and prints its spelling, if the number is greater than 5 print entered number is greater than 5
+
+void main() {
+  int x=6;
+  if(x==0){
+    print("Zero");
+  }
+  if(x==1){
+    print("One");
+  }
+  if(x==2){
+    print("Two");
+  }
+  if(x==3){
+    print("Three");
+  }
+  if(x==4){
+    print("Four");
+  }
+  if(x==5){
+    print("Five");
+  }
+  else{
+    print("Entered number is greater than 5");
+  }
+}
