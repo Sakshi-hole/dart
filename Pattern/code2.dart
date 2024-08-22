8b478bcd1f551080974662025cc59a75b6b82d23
Sakshi-hole <sakshihole2105@gmail.com>
2024-08-22 15:42:55 +0530

    pattern

---
+++ Pattern/code2.dart
@@ -0,0 +1,17 @@
+/* Write a program to print the following pattern
+Number of rows = 3
+INC INC INC
+INC INC INC
+INC INC INC*/
+
+import "dart:io";
+void main(){
+  print("Enter Number of rows:");
+  int rows=int.parse(stdin.readLineSync()!);
+  for(int i=0;i<rows;i++){
+    for(int j=0;j<rows;j++){
+      stdout.write("INC ");
+      }
+      print("");
+    }
+}
