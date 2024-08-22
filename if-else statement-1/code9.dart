c5d9be14f88ae891b977c1ef233914bf407fce0c
Sakshi-hole <sakshihole2105@gmail.com>
2024-08-22 15:34:56 +0530

    if else

---
+++ if-else statement-1/code9.dart
@@ -0,0 +1,26 @@
+// Create a dart program to calculate the ticket price for the upcoming Cricket World Cup
+
+/*1. For Upper Stand ticket ( represented by 1) price is
+2000
+2. For Middle Stand ticket (represented by 2) price is
+3000
+3. For Lower Stand ticket (represented by 3 ) price is
+7000
+4. All other tickets for 2500*/
+
+void main(){
+int x= 9;
+if(x==1){
+print("Please Pay 2000 rupees");
+}
+else if(x==2){
+print("Please pay 3000 rupees");
+}
+
+else if(x==3){
+print("Please pay 7000 rupees");
+}
+else{
+print("Please pay 2500 rupees");
+}
+}
