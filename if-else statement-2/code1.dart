6818af2953db345ef99c0697a83384844447c806
Sakshi-hole <sakshihole2105@gmail.com>
2024-08-22 15:37:19 +0530

    if else

---
+++ if-else statement-2/code1.dart
@@ -0,0 +1,10 @@
+// Write a program to find out whether you can run flutter project on your laptop or not
+
+void main(){
+  int ramSize=6;
+  if(ramSize<4){
+    print("Can't run a project");
+    }else{
+      print("Can run a flutter project");
+}
+}
