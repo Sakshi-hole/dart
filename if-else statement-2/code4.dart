4d45ebea7a49dfc5d1aab8baef87f1f5d4f6630f
Sakshi-hole <sakshihole2105@gmail.com>
2024-08-22 15:38:37 +0530

    if else

---
+++ if-else statement-2/code4.dart
@@ -0,0 +1,10 @@
+// Write a dart program, to check whether a number is greater than equal to 16 and is divisible by 2.
+
+void main(){
+  int x=21;
+  if(x>=16 && x%2==0){
+    print("Correct Number");
+    }else{
+      print("Incorrect Number");
+      }
+    }
