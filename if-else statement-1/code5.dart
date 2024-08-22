9ebcdaade18ff8ca76bbb995a972d4928c2c2174
Sakshi-hole <sakshihole2105@gmail.com>
2024-08-22 15:33:05 +0530

    if else

---
+++ if-else statement-1/code5.dart
@@ -0,0 +1,19 @@
+// write a program to check if a character is a vowel or consonant
+
+void main() {
+  String ch = "N";
+  if (ch == "A" ||
+      ch == "E" ||
+      ch == "I" ||
+      ch == "O" ||
+      ch == "U" ||
+      ch == "a" ||
+      ch == "e" ||
+      ch == "i" ||
+      ch == "o" ||
+      ch == "u") {
+    print("Vowel");
+  } else {
+    print("Consonant");
+  }
+}
