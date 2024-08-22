b3b6d01167890c3ca2ef3a5303e15c635c3503cc
Sakshi-hole <sakshihole2105@gmail.com>
2024-08-22 15:38:16 +0530

    if else

---
+++ if-else statement-2/code3.dart
@@ -0,0 +1,10 @@
+// Write a dart program, take a number, and print whether number is in range 30 to 50.
+
+void main(){
+  int x=55;
+  if(x>=30 && x<=50){
+    print("Number in correct range");
+    }else{
+      print("Invalid Range");
+      }
+    }
