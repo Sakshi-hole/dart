2e61149d5203871e5684f323aed54a6ff0ac2090
Sakshi-hole <sakshihole2105@gmail.com>
2024-08-22 15:31:47 +0530

    if else

---
+++ if-else statement-1/code3.dart
@@ -0,0 +1,10 @@
+//Write a dart program to determine if the user can cast a vote or not(a person with age above 18 can cast a vote )
+
+void main() {
+  int age = 17;
+  if (age >= 18) {
+    print("You can cast a vote");
+  } else {
+    print("You can't cast a vote");
+  }
+}
