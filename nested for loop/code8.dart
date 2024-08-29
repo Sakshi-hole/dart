c26f2d3ce94717f1ee3eccd1d0eb521c55484eaa
Sakshi-hole <sakshihole2105@gmail.com>
2024-08-29 10:10:39 +0530

    nested for loop

--- nested for loop/code8.dart
+++ nested for loop/code8.dart
@@ -1,17 +1,16 @@
 import 'dart:io';
+
 void main() {
-  int row = 5; 
-  int startNumber = 1; 
-  // Outer loop to handle the number of rows
+  int row = 5;
+  int startNumber = 1;
+
   for (int i = 1; i <= row; i++) {
-    // Inner loop to print numbers in each row
     for (int j = 0; j < i; j++) {
-      stdout.write("${startNumber + j} "); // Print the current number followed by a space
+      stdout.write("${startNumber + j} ");
     }
-    // Move to the next line after printing all numbers for the current row
+
     print("");
 
-    // Update the starting number for the next row
     startNumber += i;
   }
 }
