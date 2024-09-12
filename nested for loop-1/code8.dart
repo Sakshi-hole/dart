c38dd25dbcca5f1e319f957b5ded187c0bd504cf
Sakshi-hole <sakshihole2105@gmail.com>
2024-09-12 15:39:35 +0530

    nested for loop

---
+++ nested for loop-1/code8.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main() {
+  int row = 5;
+  int startNumber = 1;
+
+  for (int i = 1; i <= row; i++) {
+    for (int j = 0; j < i; j++) {
+      stdout.write("${startNumber + j} ");
+    }
+
+    print("");
+
+    startNumber += i;
+  }
+}
