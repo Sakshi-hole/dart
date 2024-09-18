3ac30803cbd390299a26437529e96bca57f7ac71
Sakshi-hole <sakshihole2105@gmail.com>
2024-09-18 23:26:30 +0530

    number system

---
+++ number system/code8.dart
@@ -0,0 +1,22 @@
+// WRITE A CODE TO PRINT THE NUMBER IS DUCK NUMBER OR NOT
+
+
+import "dart:io";
+void main(){
+  print("Enter any number");
+  int num=int.parse(stdin.readLineSync()!);
+  int flag=0;
+  while(num>0){
+    int rem=num%10;
+    if(rem==0){
+      flag=1;
+    }
+    num=num~/10;
+    }
+    if(flag==1){
+      print("It's Duck no");
+    }
+    else{
+      print("It's not Duck no");
+    }
+}
