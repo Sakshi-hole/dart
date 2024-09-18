124b6b704319d78239362e48b88a7e11f62c267d
Sakshi-hole <sakshihole2105@gmail.com>
2024-09-18 23:25:54 +0530

    number system

---
+++ number system/code5.dart
@@ -0,0 +1,20 @@
+// WRITE A CODE TO PRINT THE NUMBER IS PALINDROME NUMBER OR NOT
+
+import "dart:io";
+void main(){
+  print("Enter any number");
+  int num=int.parse(stdin.readLineSync()!);
+  int temp=num;
+  int num2=0;
+  while(num>0){
+    int rem=num%10;
+    num2=num2*10+rem;
+    num=num~/10;
+  }
+  if(temp==num2){
+    print("The no is Pallindrom");
+  }
+  else{
+    print("The number is not pallindrom");
+  }
+}
