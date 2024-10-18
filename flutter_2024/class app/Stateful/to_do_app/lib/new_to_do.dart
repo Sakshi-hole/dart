1648bd4c772e412b184ada896f54e6ada263dc64
Sakshi-hole <sakshihole2105@gmail.com>
2024-10-18 13:24:28 +0530

    to_do_list

---
+++ flutter_2024/class app/Stateful/to_do_app/lib/new_to_do.dart
@@ -0,0 +1,441 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import "package:to_do_app/todo_model_class.dart";
+
+class ToDoApp extends StatefulWidget {
+  const ToDoApp({super.key});
+  @override
+  State<ToDoApp> createState() => _ToDoAppState();
+}
+
+class _ToDoAppState extends State<ToDoApp> {
+  TextEditingController dateController = TextEditingController();
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+  void showBottomSheet(bool doedit, [ToDoModelClass? toDoModelObj]) {
+    showModalBottomSheet(
+        isScrollControlled: true,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30.0),
+            topRight: Radius.circular(30.0),
+          ),
+        ),
+        isDismissible: true,
+        context: context,
+        builder: (context) {
+          return Padding(
+            padding: EdgeInsets.only(
+              left: 20,
+              right: 20,
+
+              //avoid keyboard overlap
+              bottom: MediaQuery.of(context).viewInsets.bottom,
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  "Create Task",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 22,
+                  ),
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text(
+                      "Title",
+                      style: TextStyle(
+                        color: Color.fromRGBO(0, 139, 148, 1),
+                        fontWeight: FontWeight.w400,
+                        fontSize: 15,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 3,
+                    ),
+                    TextField(
+                      controller: titleController,
+                      decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: const BorderSide(
+                            color: Color.fromRGBO(0, 139, 148, 1),
+                          ),
+                        ),
+                        border: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.purpleAccent),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    const Text(
+                      "Description",
+                      style: TextStyle(
+                        color: Color.fromRGBO(0, 139, 148, 1),
+                        fontWeight: FontWeight.w400,
+                        fontSize: 15,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 3,
+                    ),
+                    TextField(
+                      controller: descriptionController,
+                      maxLines: 4,
+                      decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: const BorderSide(
+                            color: Color.fromRGBO(0, 139, 148, 1),
+                          ),
+                        ),
+                        border: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.purpleAccent),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    const Text(
+                      "Date",
+                      style: TextStyle(
+                        color: Color.fromRGBO(0, 139, 148, 1),
+                        fontWeight: FontWeight.w400,
+                        fontSize: 15,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 3,
+                    ),
+                    TextField(
+                      controller: dateController,
+                      readOnly: true,
+                      decoration: InputDecoration(
+                        suffixIcon: const Icon(Icons.date_range_rounded),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: const BorderSide(
+                            color: Color.fromRGBO(0, 139, 148, 1),
+                          ),
+                        ),
+                        border: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.purpleAccent),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      onTap: () async {
+                        DateTime? pickeddate = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2024),
+                          lastDate: DateTime(2025),
+                        );
+                        String formatedDate =
+                            DateFormat.yMMMd().format(pickeddate!);
+                        setState(() {
+                          dateController.text = formatedDate;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 50,
+                  width: 300,
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(30)),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      backgroundColor: const Color.fromRGBO(0, 139, 148, 1),
+                    ),
+                    onPressed: () {
+                      doedit ? submit(doedit, toDoModelObj) : submit(doedit);
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text(
+                      "Submit",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+              ],
+            ),
+          );
+        });
+  }
+
+//list of images
+
+  /* List<String> imageUrls = [
+    "https://static.vecteezy.com/system/resources/previews/015/337/676/non_2x/transparent-like-icon-free-png.png",
+    "https://example.com/your-second-image.png", // Replace with your second image URL
+    "https://example.com/your-third-image.png", // Replace with your third image URL
+    "https://example.com/your-fourth-image.png", // Replace with your fourth image URL
+  ];*/
+
+  var listOfColors = [
+    const Color.fromRGBO(250, 232, 232, 1),
+    const Color.fromRGBO(232, 237, 250, 1),
+    const Color.fromRGBO(250, 249, 232, 1),
+    const Color.fromRGBO(250, 232, 250, 1),
+  ];
+  List<ToDoModelClass> todoList = [
+    ToDoModelClass(
+      title: "Java",
+      description: "Abstraction, Inheritance, Polymorphism, Encapsulation ",
+      date: "17 july 2024",
+    ),
+    ToDoModelClass(
+      title: "Python ",
+      description:
+          "Dictionaries, Tuples, Exception, function, Lambda function, Collections ",
+      date: "18 July 2024",
+    ),
+  ];
+  void submit(bool doedit, [ToDoModelClass? toDoModelObj]) {
+    if (titleController.text.trim().isNotEmpty &&
+        descriptionController.text.trim().isNotEmpty &&
+        dateController.text.trim().isNotEmpty) {
+      if (!doedit) {
+        setState(() {
+          todoList.add(
+            ToDoModelClass(
+              title: titleController.text.trim(),
+              description: descriptionController.text.trim(),
+              date: dateController.text.trim(),
+            ),
+          );
+        });
+      } else {
+        setState(() {
+          toDoModelObj!.date = dateController.text.trim();
+          toDoModelObj.title = titleController.text.trim();
+          toDoModelObj.description = descriptionController.text.trim();
+        });
+      }
+    }
+    clearController();
+  }
+
+//clear
+  void clearController() {
+    titleController.clear();
+    descriptionController.clear();
+    dateController.clear();
+  }
+
+  //remove
+  void removeTasks(ToDoModelClass toDoModelObj) {
+    setState(() {
+      todoList.remove(toDoModelObj);
+    });
+  }
+
+  void editTask(ToDoModelClass toDoModelObj) {
+    titleController.text = toDoModelObj.title;
+    descriptionController.text = toDoModelObj.description;
+    dateController.text = toDoModelObj.date;
+    showBottomSheet(true, toDoModelObj);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    titleController.dispose();
+    dateController.dispose();
+    descriptionController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      appBar: AppBar(
+        backgroundColor: const Color.fromRGBO(2, 167, 177, 1),
+        centerTitle: true,
+        title: const Text(
+          "To-do App",
+          style: TextStyle(
+            fontWeight: FontWeight.w700,
+            fontSize: 25,
+          ),
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: todoList.length,
+        itemBuilder: (context, index) {
+          ///GET THE IMAGE
+          ///
+          ///
+          //String imageUrl = imageUrls[index % imageUrls.length];
+
+          return Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 15,
+              vertical: 16,
+            ),
+            child: Container(
+              decoration: BoxDecoration(
+                color: listOfColors[index % listOfColors.length],
+                boxShadow: const [
+                  BoxShadow(
+                    offset: Offset(0, 10),
+                    color: Color.fromRGBO(0, 0, 0, 0.1),
+                    blurRadius: 10,
+                  )
+                ],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          height: 70,
+                          width: 70,
+                          decoration: const BoxDecoration(
+                            color: Colors.white,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Image.network(
+                            "https://static.vecteezy.com/system/resources/previews/015/337/676/non_2x/transparent-like-icon-free-png.png",
+                          ),
+
+                          /* child: const Icon(
+                            Icons.image_outlined,
+                            color: Colors.grey,
+                            size: 50,
+                          ),*/
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                todoList[index].title,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 15,
+                                  color: Color.fromRGBO(0, 0, 0, 1),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                todoList[index].description,
+                                style: const TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w500,
+                                  color: Color.fromRGBO(84, 84, 84, 1),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 14.0,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                      child: Row(
+                        children: [
+                          Text(
+                            todoList[index].date,
+                            style: const TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w400,
+                              color: Color.fromRGBO(132, 132, 132, 1),
+                            ),
+                          ),
+                          const Spacer(),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  editTask(todoList[index]);
+                                },
+                                child: const Icon(
+                                  Icons.edit_outlined,
+                                  color: Color.fromRGBO(0, 139, 148, 1),
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  removeTasks(todoList[index]);
+                                },
+                                child: const Icon(
+                                  Icons.delete_outline,
+                                  color: Color.fromRGBO(0, 139, 148, 1),
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: const Color.fromRGBO(0, 139, 148, 1),
+        onPressed: () {
+          clearController();
+          showBottomSheet(false);
+        },
+        child: const Icon(
+          size: 50,
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
